eea8d2174f684188445c7bbd06256d07a9454055
Mark Saroufim <marksaroufim@meta.com>
2023-10-07 18:44:00 -0700

    Update run_benchmark.py

--- pyperformance/data-files/benchmarks/bm_asyncio_websockets/run_benchmark.py
+++ pyperformance/data-files/benchmarks/bm_asyncio_websockets/run_benchmark.py
@@ -9,6 +9,7 @@ import pyperf
 import websockets.server
 import websockets.client
 import asyncio
+import platform
 
 CHUNK_SIZE = 1024 ** 2
 DATA = b"x" * CHUNK_SIZE
@@ -16,12 +17,17 @@ DATA = b"x" * CHUNK_SIZE
 stop: asyncio.Event
 
 
+# Update timeout to 120 seconds for Windows
+if platform.system() == "Windows":
+    TIMEOUT = 120  
+else:
+    TIMEOUT = None
+
 async def handler(websocket) -> None:
     for _ in range(100):
         await websocket.recv()
 
     stop.set()
-
 
 async def main() -> None:
     global stop
@@ -30,9 +36,8 @@ async def main() -> None:
     async with websockets.server.serve(handler, "", 8001):
         async with websockets.client.connect("ws://localhost:8001") as ws:
             await asyncio.gather(*[ws.send(DATA) for _ in range(100)])
-        await stop.wait()
+        await asyncio.wait_for(stop.wait(), timeout=TIMEOUT)
     return pyperf.perf_counter() - t0
-
 
 if __name__ == "__main__":
     runner = pyperf.Runner()
